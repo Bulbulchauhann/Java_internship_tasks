fd2af194487a3eaca195a6aa1d34abb2710fef4a
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-05-27 12:19:10 +0530

    Add files via upload

---
+++ Classes.java
@@ -0,0 +1,21 @@
+class Students{
+    //  String name;
+    //  String roll;
+    //  int marks;
+     String name = "Nashit";
+     String roll = "1";
+     int[] marks = {100, 89, 88, 90};
+     String email = "Nashit@nashit.com";
+     String hello = "aZihfio8u9@bu11";
+     char a = '@';
+     float b = 6.5f;
+     boolean l = true;
+}
+
+public class Classes {
+    public static void main(String[] args) {
+       Students s1 = new Students();
+        // int a, b, c;
+        // System.out.println("nashit");
+    }
+}
