874a9289031c4fc694bac94572585dae322a03c6
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-06-07 09:55:51 +0530

    JDBC Database creation
    Connecting MYSql with JDBC, creating connection, creating database, creating table, inserting and reading data.

--- eclipse1/src/conn/conn1.java
+++ eclipse1/src/conn/conn1.java
@@ -2,19 +2,17 @@ package conn;
 import java.sql.*;
 
 public class conn1 {
-	public static void main(String[] args) throws ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
-		String user = "root";
-		String pass = "";
+	public static void main(String[] args)  {
+//		Class.forName("com.mysql.jdbc.Driver");
+//		String user = "root";
+//		String pass = "";
 		try {
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "2299");
+		conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 //		Statement st = conn.createStatement();
-		System.out.println("Connected");	
-		
+		System.out.println("Connected");
 	}
-
 }
- 
