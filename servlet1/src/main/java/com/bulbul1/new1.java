7c42ff17a8a20277c81e31166172355863567931
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-06-28 11:51:17 +0530

    Servlet 1

---
+++ servlet1/src/main/java/com/bulbul1/new1.java
@@ -0,0 +1,21 @@
+package com.bulbul1;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+
+public class new1  extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res){
+		
+		String a = req.getParameter("fname");
+		String b = req.getParameter("pass");
+		
+        System.out.println(a + "  " + b);
+		
+	    
+	    
+	}
+}
