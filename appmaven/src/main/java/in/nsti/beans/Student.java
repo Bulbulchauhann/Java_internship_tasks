07f7ec8516e95317a5e05c15ebf8749568a7eebc
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-07-25 14:46:16 +0530

    25/072024
    Maven Quickstart

---
+++ appmaven/src/main/java/in/nsti/beans/Student.java
@@ -0,0 +1,26 @@
+package in.nsti.beans;
+
+public class Student {
+	private String name;
+	private String mail;
+	private String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
