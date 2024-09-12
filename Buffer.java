46c887dc4056c483a62d408f4f41adf4fef3493f
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-09-12 20:21:16 +0530

    Update Buffer.java

--- Buffer.java
+++ Buffer.java
@@ -1,6 +1,5 @@
 public class Buffer {
     public static void main(String[] args) {
-        // StringBuffer a = new StringBuffer();
         // StringBuffer a = new StringBuffer("Bulbul");
         // System.out.println(a.capacity());
 
