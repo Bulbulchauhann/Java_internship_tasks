9ed10a0629e9ce1058e11107ff54d3fac4b70602
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-09-16 16:11:19 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -2,7 +2,7 @@ package com.bulbul;
 
 public class Basics {
     public static void main(String[] args) {
-//        int a = 10;
+       // int a = 10;
 //        if (a == 10) {
 //            System.out.println("Hello World");
 //        }
