9db536ea56f74e5c3c964f4e0280fd7ccbe3bfc4
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-09-20 21:36:49 +0530

    Update new1.java

--- new1/new1.java
+++ new1/new1.java
@@ -1,6 +1,6 @@
 public class new1{
     public static void main(String[] args) {
-        final int age = 10; //final cannot be reassigned
+        final int age = 10; //final cannot be reassigne
         age = 20;
         // String name = "Bulbul";
         // name = "peter";
