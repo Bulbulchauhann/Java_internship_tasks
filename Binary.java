9e5e78478c24f161826a3291ffe4c80c5739ec5f
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-09-29 14:18:16 +0530

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -6,13 +6,10 @@ public class Binary {
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
-
         System.out.println("Enter a Number :: ");
 
         int a = in.nextInt();
-
         String b = Integer.toBinaryString(a);
-
         System.out.println("Converted Binary Number ::" + b);
     }
 }
