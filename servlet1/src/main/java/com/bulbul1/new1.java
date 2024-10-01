02afc59c7ed084c0a9e944d359809d166cf024e9
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-10-01 13:07:27 +0530

    Update new1.java

--- servlet1/src/main/java/com/bulbul1/new1.java
+++ servlet1/src/main/java/com/bulbul1/new1.java
@@ -5,14 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-
 public class new1  extends HttpServlet{
-
-	public void service(HttpServletRequest req, HttpServletResponse res){
-		
+	public void service(HttpServletRequest req, HttpServletResponse res){		
 		String a = req.getParameter("fname");
-		String b = req.getParameter("pass");
-		
+		String b = req.getParameter("pass");		
         System.out.println(a + "  " + b);
 		
 	    
