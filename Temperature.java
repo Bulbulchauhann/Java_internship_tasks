1a2d24836448e971b225c3f9cfe15c519300b076
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-10-15 23:19:27 +0530

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -6,8 +6,7 @@ public class Temperature {
         Scanner in = new Scanner(System.in);
         System.out.println("Please Enter temp in C: ");
         float tempC = in.nextFloat();
-        float tempF = (tempC * 9/5)  + 32;
-        
+        float tempF = (tempC * 9/5)  + 32;        
         System.out.println(tempF);
     }
 }
